090fddafa567669e4f86dab0e54b5262154e6633
EC2 Default User <ec2-user@ip-172-31-1-75.ap-northeast-1.compute.internal>
2021-01-15 07:57:07 +0000

    create 実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,13 +1,21 @@
 class ItemsController < ApplicationController
     
     def index
-     @items = Item.order("created_at DESC")
+         @items = Item.order("created_at DESC")
     end
 
     def new
+        @item = Item.new
     end
     
+    def create
+        Item.create(name: item_params[:name])
+    end
     
+    private
+    def item_params
+        params.require(:item).permit(:name)
+    end
     
     
 end
